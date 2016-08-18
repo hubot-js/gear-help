9730a4589cd9b1b01ccad6b0a455c45c11016943
robsonbittencourt <robson.luizv@gmail.com>
2016-08-18 02:44:15 -0300

    Create help feature

---
+++ src/handlers/help.js
@@ -0,0 +1,53 @@
+'use strict';
+
+exports.handle = handle;
+
+function handle(hubot, message) {
+   hubot.postMessage(hubot.getRecipient(message), getHelpMessage(hubot, message), {as_user: true});   
+}
+
+function getHelpMessage(hubot, message) {
+   var user = hubot._getUserById(message.user);
+   
+   if (hubot._isChannelConversation(message)) {
+      return getPublicHelpMessage(hubot, user);
+   } 
+   
+   if (hubot._isPrivateConversation(message)) {
+      return getPrivateHelpMessage(hubot, user);
+   }
+}
+
+function getPublicHelpMessage(hubot, user) {
+   return hubot.speech().hello(user).append('You need help? Call me in private chat.').end();
+}
+
+function getPrivateHelpMessage(hubot, user) {
+   return hubot.speech().hello(user).append('How can I help?').append(getHelpOptions(hubot)).end();
+}
+
+function getHelpOptions(hubot) {
+   var speecher = hubot.speech();
+
+   getVisibleCategories(hubot).forEach(category => buildCategory(hubot, speecher, category));
+
+   return speecher.end();
+}
+
+function getVisibleCategories(hubot) {
+   return hubot.core.categories.filter(c => c.visible);
+}
+
+function buildCategory(hubot, speecher, category) {
+   speecher.paragraph().bold(category.name).line().append(category.description).paragraph();
+   
+   buildTasks(hubot, speecher, category);
+}
+
+function buildTasks(hubot, speecher, category) {
+   getTasks(hubot, category).forEach(task => speecher.item().bold(task.trigger).separator().append(task.description).line());
+}
+
+function getTasks(hubot, category) {
+   return hubot.core.tasks.filter(t => t.category == category.key);
+}
---
+++ test/handlers/help.js
@@ -0,0 +1,152 @@
+var help = require('../../src/handlers/help');
+var chai = require("chai");
+var expect = chai.expect;
+var sinon = require('sinon');
+
+describe('Help ', function() {
+   var message, hubot, speech, endSpy, boldSpy, lineSpy, itemSpy, helloSpy, appendSpy, separatorSpy, paragraphSpy, postMessageSpy;
+
+   beforeEach(function() {
+      message = { "user": "hubot", "channel": "myChannel" };
+      
+      hubot = getHubot();
+      speech = getSpeech();
+
+      initSpies();
+   });
+
+   describe('requested in a channel', function() {
+
+      it("post a message saying that assistance should be requested in private", function() {
+         isChannelConversation();
+
+         help.handle(hubot, message);
+
+         expect(helloSpy.calledWith(message.user)).to.be.true;
+         expect(appendSpy.calledWith('You need help? Call me in private chat.')).to.be.true;
+         expect(endSpy.called).to.be.true;
+
+         expect(postMessageSpy.calledWith(message.channel, '', {as_user: true})).to.be.true;
+      });
+      
+   });   
+
+   describe('requested in privated', function() {
+
+      describe('post a message showing', function() {
+
+         it("only visible categories", function() {
+            isPrivateConversation();
+            withoutTasks();
+
+            help.handle(hubot, message);
+
+            expect(helloSpy.withArgs(message.user).calledOnce).to.be.true;
+            expect(appendSpy.withArgs('How can I help?').calledOnce).to.be.true;
+
+            expect(paragraphSpy.callCount).to.be.equal(4);
+            expect(lineSpy.callCount).to.be.equal(2);
+            
+            expect(boldSpy.calledWith('name1')).to.be.true;
+            expect(boldSpy.calledWith('name2')).to.be.true;
+            
+            expect(appendSpy.calledWith('description1')).to.be.true;
+            expect(appendSpy.calledWith('description2')).to.be.true;
+
+            expect(boldSpy.neverCalledWith('category3')).to.be.true;
+            expect(appendSpy.neverCalledWith('description3')).to.be.true;
+
+            expect(endSpy.callCount).to.be.equal(2);
+
+            expect(postMessageSpy.calledWith(message.user, '', {as_user: true})).to.be.true;
+         });
+
+         it("and tasks by category", function() {
+            isPrivateConversation();
+
+            help.handle(hubot, message);
+
+            expect(itemSpy.callCount).to.be.equal(4);
+            expect(separatorSpy.callCount).to.be.equal(4);
+            expect(lineSpy.callCount).to.be.equal(6);
+
+            expect(boldSpy.calledWith('trigger1')).to.be.true;
+            expect(boldSpy.calledWith('trigger2')).to.be.true;
+            expect(boldSpy.calledWith('trigger3')).to.be.true;
+            expect(boldSpy.calledWith('trigger4')).to.be.true;
+
+            expect(appendSpy.calledWith('taskDescription1')).to.be.true;
+            expect(appendSpy.calledWith('taskDescription2')).to.be.true;
+            expect(appendSpy.calledWith('taskDescription3')).to.be.true;
+            expect(appendSpy.calledWith('taskDescription4')).to.be.true;
+
+            expect(postMessageSpy.calledWith(message.user, '', {as_user: true})).to.be.true;
+         });
+
+      });
+   });
+
+   function getHubot() {
+      return { 
+         _getUserById: function () { return message.user },
+         postMessage: function () {},
+         speech: function() { return speech },
+         core: {
+            categories: [
+               { "key": "category1", "name": "name1", "description": "description1", "visible": true },
+               { "key": "category2", "name": "name2", "description": "description2", "visible": true },
+               { "key": "category3", "name": "name3", "description": "description3", "visible": false }
+            ],
+            tasks: [
+               { "category": "category1", "trigger": "trigger1", "description": "taskDescription1" },
+               { "category": "category1", "trigger": "trigger2", "description": "taskDescription2" },
+               { "category": "category2", "trigger": "trigger3", "description": "taskDescription3"  },
+               { "category": "category2", "trigger": "trigger4", "description": "taskDescription4"  },
+            ]
+         }
+      };
+   }
+
+   function getSpeech() {
+      return {
+         end: function() { return '' },
+         bold: function() { return this },
+         line: function() { return this },
+         item: function() { return this },
+         hello: function() { return this },
+         append: function() { return this },
+         separator: function() { return this },
+         paragraph: function() { return this }
+      }
+   }
+
+   function initSpies() {
+      endSpy = sinon.spy(speech, "end");
+      boldSpy = sinon.spy(speech, "bold");
+      lineSpy = sinon.spy(speech, "line");
+      itemSpy = sinon.spy(speech, "item");
+      helloSpy = sinon.spy(speech, "hello");
+      appendSpy = sinon.spy(speech, "append");
+      separatorSpy = sinon.spy(speech, "separator");
+      paragraphSpy = sinon.spy(speech, "paragraph");
+      
+      postMessageSpy = sinon.spy(hubot, "postMessage");
+   }
+
+   function isChannelConversation() {
+      hubot._isChannelConversation = function () { return true };
+      hubot._isPrivateConversation = function () { return false };
+      hubot.getRecipient = function() { return message.channel };
+   }
+
+   function isPrivateConversation() {
+      hubot._isChannelConversation = function () { return false };
+      hubot._isPrivateConversation = function () { return true };
+      hubot.getRecipient = function() { return message.user };   
+   }
+
+   function withoutTasks() {
+      hubot.core.tasks = [];
+   }
+
+}); 
