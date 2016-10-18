15ac68d7ded6050d5e84038a846a9cbba5d64c06
Robson <robson.luizv@gmail.com>
2016-10-18 21:59:10 -0200

    Fix #14 Add eslint e fix issues

--- src/handlers/help.js
+++ src/handlers/help.js
@@ -3,37 +3,37 @@
 exports.handle = handle;
 
 function handle(hubot, message) {
-   const user = hubot.getUser(message);
-   const helpMessage = hubot.speech().hello(user).append('How can I help?').append(getHelpOptions(hubot)).end();
-   
-   hubot.speak(message, helpMessage);
+  const user = hubot.getUser(message);
+  const helpMessage = hubot.speech().hello(user).append('How can I help?').append(getHelpOptions(hubot)).end();
+
+  hubot.speak(message, helpMessage);
 }
 
 function getHelpOptions(hubot) {
-   const speecher = hubot.speech();
+  const speecher = hubot.speech();
 
-   activeGears(hubot).forEach(gear => buildCategories(gear, speecher));
+  activeGears(hubot).forEach(gear => buildCategories(gear, speecher));
 
-   return speecher.end();
+  return speecher.end();
 }
 
 function activeGears(hubot) {
-   return hubot.gears.filter(g => g.active);
+  return hubot.gears.filter(g => g.active);
 }
 
 function buildCategories(gear, speecher) {
-   const visibleCategories = gear.categories.filter(c => c.visible);
+  const visibleCategories = gear.categories.filter(c => c.visible);
 
-   visibleCategories.forEach(function(category) {
-      speecher.paragraph().bold(category.name).line().append(category.description).paragraph();
-      buildTasks(gear, category, speecher);
-   });  
+  visibleCategories.forEach((category) => {
+    speecher.paragraph().bold(category.name).line().append(category.description).paragraph();
+    buildTasks(gear, category, speecher);
+  });
 }
 
 function buildTasks(gear, category, speecher) {
-   tasks(gear, category).forEach(task => speecher.item().bold(task.trigger).separator().append(task.description).line());
+  tasks(gear, category).forEach(task => speecher.item().bold(task.trigger).separator().append(task.description).line());
 }
 
 function tasks(gear, category) {
-   return gear.tasks.filter(t => t.category === category.key);
+  return gear.tasks.filter(t => t.category === category.key);
 }
--- test/handlers/help.js
+++ test/handlers/help.js
@@ -1,151 +1,148 @@
-const help = require('../../src/handlers/help');
-const chai = require("chai");
-const expect = chai.expect;
+/* eslint-disable no-unused-expressions */
+
+'use strict';
+
+const chai = require('chai');
 const sinon = require('sinon');
 
-describe('Help post a message showing', function() {
-   let message, hubot, speech, endSpy, boldSpy, lineSpy, itemSpy, helloSpy, appendSpy, separatorSpy, paragraphSpy, speakSpy;
+const help = require('../../src/handlers/help');
 
-   beforeEach(function() {
-      message = { "user": "hubot", "channel": "myChannel" };
-      
-      hubot = getHubot();
-      speech = getSpeech();
+const expect = chai.expect;
 
-      initSpies();
-   });
-   
-   it("only categories of active gears", function() {
-      isPrivateConversation();
-      withoutTasks();
+describe('Help post a message showing', () => {
+  let message;
+  let hubot;
+  let speech;
+  let endSpy;
+  let boldSpy;
+  let lineSpy;
+  let itemSpy;
+  let helloSpy;
+  let appendSpy;
+  let separatorSpy;
+  let paragraphSpy;
+  let speakSpy;
 
-      help.handle(hubot, message);
+  beforeEach(() => {
+    message = { user: 'hubot', channel: 'myChannel' };
 
-      expect(boldSpy.calledWith('name1')).to.be.true;
-      expect(boldSpy.neverCalledWith('category4')).to.be.true;
-   });
+    hubot = getHubot();
+    speech = getSpeech();
 
-   it("only visible categories", function() {
-      isPrivateConversation();
-      withoutTasks();
+    initSpies();
+  });
 
-      help.handle(hubot, message);
+  it('only categories of active gears', () => {
+    withoutTasks();
 
-      expect(helloSpy.withArgs(message.user).calledOnce).to.be.true;
-      expect(appendSpy.withArgs('How can I help?').calledOnce).to.be.true;
+    help.handle(hubot, message);
 
-      expect(paragraphSpy.callCount).to.be.equal(4);
-      expect(lineSpy.callCount).to.be.equal(2);
-      
-      expect(boldSpy.calledWith('name1')).to.be.true;
-      expect(boldSpy.calledWith('name2')).to.be.true;
-      
-      expect(appendSpy.calledWith('description1')).to.be.true;
-      expect(appendSpy.calledWith('description2')).to.be.true;
+    expect(boldSpy.calledWith('name1')).to.be.true;
+    expect(boldSpy.neverCalledWith('category4')).to.be.true;
+  });
 
-      expect(boldSpy.neverCalledWith('name3')).to.be.true;
-      expect(appendSpy.neverCalledWith('description3')).to.be.true;
+  it('only visible categories', () => {
+    withoutTasks();
 
-      expect(endSpy.callCount).to.be.equal(2);
+    help.handle(hubot, message);
 
-      expect(speakSpy.calledWith(message, 'fakeMessage')).to.be.true;
-   });
+    expect(helloSpy.withArgs(message.user).calledOnce).to.be.true;
+    expect(appendSpy.withArgs('How can I help?').calledOnce).to.be.true;
 
-   it("tasks by category", function() {
-      isPrivateConversation();
+    expect(paragraphSpy.callCount).to.be.equal(4);
+    expect(lineSpy.callCount).to.be.equal(2);
 
-      help.handle(hubot, message);
+    expect(boldSpy.calledWith('name1')).to.be.true;
+    expect(boldSpy.calledWith('name2')).to.be.true;
 
-      expect(itemSpy.callCount).to.be.equal(4);
-      expect(separatorSpy.callCount).to.be.equal(4);
-      expect(lineSpy.callCount).to.be.equal(6);
+    expect(appendSpy.calledWith('description1')).to.be.true;
+    expect(appendSpy.calledWith('description2')).to.be.true;
 
-      expect(boldSpy.calledWith('trigger1')).to.be.true;
-      expect(boldSpy.calledWith('trigger2')).to.be.true;
-      expect(boldSpy.calledWith('trigger3')).to.be.true;
-      expect(boldSpy.calledWith('trigger4')).to.be.true;
+    expect(boldSpy.neverCalledWith('name3')).to.be.true;
+    expect(appendSpy.neverCalledWith('description3')).to.be.true;
 
-      expect(appendSpy.calledWith('taskDescription1')).to.be.true;
-      expect(appendSpy.calledWith('taskDescription2')).to.be.true;
-      expect(appendSpy.calledWith('taskDescription3')).to.be.true;
-      expect(appendSpy.calledWith('taskDescription4')).to.be.true;
+    expect(endSpy.callCount).to.be.equal(2);
 
-      expect(speakSpy.calledWith(message, 'fakeMessage')).to.be.true;
-   });
+    expect(speakSpy.calledWith(message, 'fakeMessage')).to.be.true;
+  });
 
-   function getHubot() {
-      return { 
-         getUser: function () { return message.user },
-         speak: function () {},
-         speech: function() { return speech },
-         gears: [
-            {
-               categories: [
-                  { "key": "category1", "name": "name1", "description": "description1", "visible": true },
-                  { "key": "category2", "name": "name2", "description": "description2", "visible": true },
-                  { "key": "category3", "name": "name3", "description": "description3", "visible": false }
-               ],
-               tasks: [
-                  { "category": "category1", "trigger": "trigger1", "description": "taskDescription1" },
-                  { "category": "category1", "trigger": "trigger2", "description": "taskDescription2" },
-                  { "category": "category2", "trigger": "trigger3", "description": "taskDescription3"  },
-                  { "category": "category2", "trigger": "trigger4", "description": "taskDescription4"  },
-               ],
-               active: true
-            },
-            {
-               categories: [
-                  { "key": "category4", "name": "name4", "description": "description4", "visible": true },
-               ],
-               active: false
-            }
-         ]
-      };
-   }
+  it('tasks by category', () => {
+    help.handle(hubot, message);
 
-   function getSpeech() {
-      return {
-         end: function() { return 'fakeMessage' },
-         bold: function() { return this },
-         line: function() { return this },
-         item: function() { return this },
-         hello: function() { return this },
-         append: function() { return this },
-         separator: function() { return this },
-         paragraph: function() { return this }
-      }
-   }
+    expect(itemSpy.callCount).to.be.equal(4);
+    expect(separatorSpy.callCount).to.be.equal(4);
+    expect(lineSpy.callCount).to.be.equal(6);
 
-   function initSpies() {
-      endSpy = sinon.spy(speech, "end");
-      boldSpy = sinon.spy(speech, "bold");
-      lineSpy = sinon.spy(speech, "line");
-      itemSpy = sinon.spy(speech, "item");
-      helloSpy = sinon.spy(speech, "hello");
-      appendSpy = sinon.spy(speech, "append");
-      separatorSpy = sinon.spy(speech, "separator");
-      paragraphSpy = sinon.spy(speech, "paragraph");
-      
-      speakSpy = sinon.spy(hubot, "speak");
-   }
+    expect(boldSpy.calledWith('trigger1')).to.be.true;
+    expect(boldSpy.calledWith('trigger2')).to.be.true;
+    expect(boldSpy.calledWith('trigger3')).to.be.true;
+    expect(boldSpy.calledWith('trigger4')).to.be.true;
 
-   function isChannelConversation() {
-      hubot.isFromChannel = function () { return true };
-      hubot.isFromPrivate = function () { return false };
-   }
+    expect(appendSpy.calledWith('taskDescription1')).to.be.true;
+    expect(appendSpy.calledWith('taskDescription2')).to.be.true;
+    expect(appendSpy.calledWith('taskDescription3')).to.be.true;
+    expect(appendSpy.calledWith('taskDescription4')).to.be.true;
 
-   function isPrivateConversation() {
-      hubot.isFromChannel = function () { return false };
-      hubot.isFromPrivate = function () { return true };
-   }
+    expect(speakSpy.calledWith(message, 'fakeMessage')).to.be.true;
+  });
 
-   function isUnknowSource() {
-      hubot.isFromChannel = function () { return false };
-      hubot.isFromPrivate = function () { return false };
-   }
+  function getHubot() {
+    return {
+      getUser () { return message.user; },
+      speak () {},
+      speech() { return speech; },
+      gears: [
+        {
+          categories: [
+                  { key: 'category1', name: 'name1', description: 'description1', visible: true },
+                  { key: 'category2', name: 'name2', description: 'description2', visible: true },
+                  { key: 'category3', name: 'name3', description: 'description3', visible: false }
+          ],
+          tasks: [
+                  { category: 'category1', trigger: 'trigger1', description: 'taskDescription1' },
+                  { category: 'category1', trigger: 'trigger2', description: 'taskDescription2' },
+                  { category: 'category2', trigger: 'trigger3', description: 'taskDescription3' },
+                  { category: 'category2', trigger: 'trigger4', description: 'taskDescription4' }
+          ],
+          active: true
+        },
+        {
+          categories: [
+                  { key: 'category4', name: 'name4', description: 'description4', visible: true }
+          ],
+          active: false
+        }
+      ]
+    };
+  }
 
-   function withoutTasks() {
-      hubot.gears[0].tasks = [];
-   }
+  function getSpeech() {
+    return {
+      end() { return 'fakeMessage'; },
+      bold() { return this; },
+      line() { return this; },
+      item() { return this; },
+      hello() { return this; },
+      append() { return this; },
+      separator() { return this; },
+      paragraph() { return this; }
+    };
+  }
 
-}); 
+  function initSpies() {
+    endSpy = sinon.spy(speech, 'end');
+    boldSpy = sinon.spy(speech, 'bold');
+    lineSpy = sinon.spy(speech, 'line');
+    itemSpy = sinon.spy(speech, 'item');
+    helloSpy = sinon.spy(speech, 'hello');
+    appendSpy = sinon.spy(speech, 'append');
+    separatorSpy = sinon.spy(speech, 'separator');
+    paragraphSpy = sinon.spy(speech, 'paragraph');
+
+    speakSpy = sinon.spy(hubot, 'speak');
+  }
+
+  function withoutTasks() {
+    hubot.gears[0].tasks = [];
+  }
+});
